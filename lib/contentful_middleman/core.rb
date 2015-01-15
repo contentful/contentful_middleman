b8d688740622723e06b5e12ed6de0123a5311efb
Farruco Sanjurjo <farruco@contentful.com>
2015-01-15 12:04:11 +0100

    Massage options describing the space

--- lib/contentful_middleman/core.rb
+++ lib/contentful_middleman/core.rb
@@ -7,7 +7,7 @@ module ContentfulMiddleman
     self.supports_multiple_instances = false
 
     option :space, nil,
-      'The Contentful Space ID'
+      'The Contentful Space ID and name'
 
     option :access_token, nil,
       'The Contentful Content Delivery API access token'
@@ -22,23 +22,43 @@ module ContentfulMiddleman
       super
 
       app.set :contentful_middleman, self
-      app.set :contentful_middleman_client, client
     end
 
     # The Contentful Gem client for the Content Delivery API
     def client
       @client ||= Contentful::Client.new(
         access_token:     options.access_token,
-        space:            options.space,
+        space:            options.space.fetch(:id),
         dynamic_entries:  :auto
       )
     end
 
+    #
+    # Middleman hooks
+    #
+    def after_configuration
+      massage_options
+
+      app.set :contentful_middleman_client, client
+    end
+
+    #
+    # Middleman helpers
+    #
     helpers do
       # A helper method to access the Contentful Gem client
       def contentful
         contentful_middleman_client
       end
+    end
+
+    private
+    def massage_options
+      space_option          = options.space
+      space_name            = space_option.keys.first
+      space_id              = space_option.fetch(space_name)
+
+      options.space = { name: space_name, id: space_id }
     end
   end
 end
